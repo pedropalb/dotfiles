2c28acf1c23b3509c9defe513e9948dcad8edbaa
Pedro A. L. Barbosa <pedro17lopes@gmail.com>
2024-11-24 19:02:02 -0300

    Add nvim configs

---
+++ private_dot_config/private_nvim/private_lua/private_custom/private_plugins/private_init.lua
@@ -0,0 +1,11 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'mrcjkb/haskell-tools.nvim',
+    version = '^4', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+}
