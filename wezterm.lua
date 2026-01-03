3f947dc03a834548b8ca9c1d2b1a161fb75d5f87
Pedro A. L. Barbosa <pedro17lopes@gmail.com>
2026-01-03 01:52:34 -0300

    feat(wez): change wezterm theme

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
 
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Github Dark'
 
 return config
 
