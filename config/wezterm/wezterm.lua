8404cb97a42c9e5367203486339b4271b43f5ea1
Pedro A. L. Barbosa <pedro17lopes@gmail.com>
2026-02-12 08:39:40 -0300

    fix(wezterm): font configuration

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,6 +5,6 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 config.color_scheme = 'Github Dark'
-config.font = wezterm.font 'MesloLGS NF'
+config.font = wezterm.font("MesloLGL Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
 
 return config
