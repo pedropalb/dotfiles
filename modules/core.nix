2a65e3d4d79bfc17f7737f6d9b8f88e88f3a879e
pedropalb <pedro17lopes@gmail.com>
2026-03-15 18:42:10 -0300

    refactor(core): aggregate home and manual expressions

--- modules/core.nix
+++ modules/core.nix
@@ -7,40 +7,38 @@
 }:
 
 {
-  home.stateVersion = "25.11";
-  home.username = username;
-  home.homeDirectory = homeDirectory;
+  home = {
+    inherit username homeDirectory;
+    stateVersion = "25.11";
+    sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+    packages = with pkgs; [
+      ripgrep
+      fd
+      bat
+      nerd-fonts.meslo-lg
+      btop
+      fastfetch
+      yazi
+      dust
+      lazygit
+      eza
+      unzip
+    ];
+    sessionPath = [
+      "${homeDirectory}/.local/bin"
+    ];
+  };
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
+  manual = {
+    manpages.enable = false;
+    html.enable = false;
+    json.enable = false;
   };
 
   systemd.user.sessionVariables = config.home.sessionVariables;
-
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-    bat
-    nerd-fonts.meslo-lg
-    btop
-    fastfetch
-    yazi
-    dust
-    lazygit
-    eza
-    unzip
-  ];
-
   fonts.fontconfig.enable = true;
-
-  home.sessionPath = [
-    "${homeDirectory}/.local/bin"
-  ];
-
   programs.home-manager.enable = true;
-
-  manual.manpages.enable = false;
-  manual.html.enable = false;
-  manual.json.enable = false;
 }
